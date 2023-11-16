8aa5a446764d3faf7eb8972ec279243f4f33853d
Damian Bravo <Dbravo1005@gmail.com>
2023-11-15 20:35:28 -0500

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+const { abstract } = require("mocha/lib/reporters/base");
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let j = word.length - 1;
+
+  for (let i = 0; i < word.length / 2; i++){
+    if (word[i] !== word[j]){
+      return false
+    }
+    j--;
 }
+  return true;
+
+}
+
+let word1 = 'abba'
+let word2 = 'racecar'
+let word3 = 'a'
+let word4 = 'robot'
+let word5 = 'ab'
+
+console.log(isPalindrome(word1));
+console.log(isPalindrome(word2));
+console.log(isPalindrome(word3));
+console.log(isPalindrome(word4));
+console.log(isPalindrome(word5));
+
+
+
 
 /* 
   Add your pseudocode here
 */
+ // make a function isPalendrome with a string (STR)
+
+ // make a variable that will keep look at the front and back of any variable
+
+ // start a for loop that will read the variable halfway and then return to read it in reverse
+
+ // if it reads the same then consider it true
+
+ // if it does not then it is false
 
 /*
   Add written explanation of your solution here
